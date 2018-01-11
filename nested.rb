a96d5a0193b48d775ee14adfba813f0f44873973
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-11 18:37:15 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -80,7 +80,7 @@ def adding_matz
           :languages => ["C"]
         }
     }
-  programmer_hash[metis] = ["katz", "fandis"]
+  programmer_hash[newly-added-to] = ["katz", "fandis"]
   programmer_hash
 
 end
